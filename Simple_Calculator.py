e986a610e51f5a9579c040ff974cd1d00982f230
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-23 13:16:56 +0800

    Final

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,73 +1,80 @@
 from tkinter import *
 
-def inClick(value):
-    screen_var.set(screen_var.get() + str(value))
-
-def clearLatest():
-    current = screen_var.get()
-    screen_var.set(current[:-1])
-    
-def clearAll():
-    screen_var.set("")
-    screen2_var.set("")
-
-def calculate():
-    try:
-        expression = screen_var.get().replace("×", "*").replace("÷", "/").replace("%", "/100")
-        result = eval(expression)
-        screen2_var.set(result)
-    except:
-        screen2_var.set("Error")
-
 mainWindow = Tk()
 mainWindow.title("Simple Calculator")
 mainWindow.geometry("314x415")  
-mainWindow.configure(bg="grey")
-mainWindow.resizable(False, False)
+mainWindow.resizable(False, False) 
+mainWindow.configure(bg="black")  #Backgorund color (i choose this because its more darker than the hex colors)
 
-# Screen variables
-screen_var = StringVar()
-screen2_var = StringVar()
+# Whthis function is when you click the buttons it will show in the Entry
+def Inclick(value):
+    calcuScreen_var.set(calcuScreen_var.get() + str(value))
 
-# Screens for calcu
-screen = Entry(mainWindow, textvariable=screen_var, font=("Arial", 35),width=12, relief="ridge", justify="right")
-screen.grid(column=1,row=1, sticky="nsew")
-screen2 = Entry(mainWindow, textvariable=screen2_var, font=("Arial", 15),width=12, relief="ridge", justify="right")
-screen2.grid(column=1,row=2, sticky="nsew")
+# This function is when you click the ⌦ it will clear the latest text (need to study it more)
+def clear_latest():
+    currentString = calcuScreen_var.get()
+    calcuScreen_var.set(currentString[:-1])
+    answerScreen_var.set("")
 
+# This function is when you click the C it will clear the all text that shows in the entry 
+def clear_all():
+    calcuScreen_var.set("")
+    answerScreen_var.set("")
+
+# This function will convert the text into operational and will compute it to show the answer (need to study it more)
+def calculate():
+    try:
+        expression = calcuScreen_var.get().replace("×", "*").replace("÷", "/").replace("%", "/100")
+        result = eval(expression)
+        answerScreen_var.set(result)
+    except:
+        answerScreen_var.set("Error")
+
+# Screen variables that i declared it into String
+calcuScreen_var = StringVar()
+answerScreen_var = StringVar()
+
+# Screens for Calculator (I used some hex colors to change just a plain black/white color to something more lighter/darker)
+calcuScreen = Entry(mainWindow,bg="#1E1E1E", fg="white", textvariable=calcuScreen_var,font=("Arial", 35),width=12, relief="ridge", justify="right")
+calcuScreen.grid(column=1,row=1, sticky="nsew")
+answerScreen = Entry(mainWindow,bg="#1E1E1E", fg="white", textvariable=answerScreen_var, font=("Arial", 15),width=12, relief="ridge", justify="right")
+answerScreen.grid(column=1,row=2, sticky="nsew")
+
+# Icon For Window
 icon = PhotoImage(file= "calc.png")    
 mainWindow.iconphoto(False, icon)
 
-# Buttons for calcu
+# Buttons for Calculator with lambda("Currently learning and trying it to practical codes")
 
-#First Group
-Button(mainWindow, text="÷",width=3, font=("Arial", 20), command=lambda: inClick("÷")).place(x=220, y=105)
-Button(mainWindow, text="%",width=3, font=("Arial", 20), command=lambda: inClick("%")).place(x=160, y=105)
-Button(mainWindow, text="⌦",width=3, font=("Arial", 20), command=lambda: clearLatest()).place(x=100, y=105)
-Button(mainWindow, text="C", width=3, font=("Arial", 20), command=lambda: clearAll()).place(x=40, y=105)
+# First Group
+Button(mainWindow, text="÷",width=3,bg="#9575CD", foreground="#FFFFFF", relief="flat", font=("Arial", 20), command=lambda: Inclick("÷")).place(x=220, y=105)
+Button(mainWindow, text="%",width=3, relief="flat",bg="#1E1E1E" ,foreground="#9575CD",font=("Arial", 20), command=lambda: Inclick("%")).place(x=160, y=105)
+Button(mainWindow, text="⌦",width=3, relief="flat",bg="#1E1E1E" ,foreground="#9575CD", font=("Arial", 20), command=lambda: clear_latest()).place(x=100, y=105)
+Button(mainWindow, text="C", width=3, relief="flat", bg="#1E1E1E" ,foreground="#9575CD", font=("Arial", 20), command=lambda: clear_all()).place(x=40, y=105)
 
 # Second Goup
-Button(mainWindow, text="×",width=3, font=("Arial", 20), command=lambda: inClick("×")).place(x=220, y=163)
-Button(mainWindow, text="9",width=3, font=("Arial", 20), command=lambda: inClick("9")).place(x=160, y=163)
-Button(mainWindow, text="8",width=3, font=("Arial", 20), command=lambda: inClick("8")).place(x=100, y=163)
-Button(mainWindow, text="7",width=3, font=("Arial", 20), command=lambda: inClick("7")).place(x=40, y=163)
+Button(mainWindow, text="×",width=3, bg="#9575CD", foreground="#FFFFFF", relief="flat",  font=("Arial", 20), command=lambda: Inclick("×")).place(x=220, y=163)
+Button(mainWindow, text="9",width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("9")).place(x=160, y=163)
+Button(mainWindow, text="8",width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("8")).place(x=100, y=163)
+Button(mainWindow, text="7",width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("7")).place(x=40, y=163)
 
 # Third Group
-Button(mainWindow, text="-", width=3, font=("Arial", 20), command=lambda: inClick("-")).place(x=220, y=221)
-Button(mainWindow, text="6", width=3, font=("Arial", 20), command=lambda: inClick("6")).place(x=160, y=221)
-Button(mainWindow, text="5",width=3, font=("Arial", 20), command=lambda: inClick("5")).place(x=100, y=221)
-Button(mainWindow, text="4",width=3, font=("Arial", 20), command=lambda: inClick("4")).place(x=40, y=221)
+Button(mainWindow, text="-", width=3,bg="#9575CD", foreground="#FFFFFF", relief="flat",  font=("Arial", 20), command=lambda: Inclick("-")).place(x=220, y=221)
+Button(mainWindow, text="6", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("6")).place(x=160, y=221)
+Button(mainWindow, text="5",width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("5")).place(x=100, y=221)
+Button(mainWindow, text="4",width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("4")).place(x=40, y=221)
 
 # Fourth Group
-Button(mainWindow, text="+", width=3, font=("Arial", 20), command=lambda: inClick("+")).place(x=220, y=279)
-Button(mainWindow, text="3", width=3, font=("Arial", 20), command=lambda: inClick("3")).place(x=160, y=279)
-Button(mainWindow, text="2", width=3, font=("Arial", 20), command=lambda: inClick("2")).place(x=100, y=279)
-Button(mainWindow, text="1", width=3, font=("Arial", 20), command=lambda: inClick("1")).place(x=40, y=279)
+Button(mainWindow, text="+", width=3,bg="#9575CD", foreground="#FFFFFF", relief="flat",  font=("Arial", 20), command=lambda: Inclick("+")).place(x=220, y=279)
+Button(mainWindow, text="3", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("3")).place(x=160, y=279)
+Button(mainWindow, text="2", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("2")).place(x=100, y=279)
+Button(mainWindow, text="1", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("1")).place(x=40, y=279)
 
 # Fourth Group
-Button(mainWindow, text="=", width=3, font=("Arial", 20), command= calculate).place(x=220, y=337)
-Button(mainWindow, text=".", width=3, font=("Arial", 20), command=lambda: inClick(".")).place(x=160, y=337)
-Button(mainWindow, text="0", width=3, font=("Arial", 20), command=lambda: inClick("0")).place(x=100, y=337)
-Button(mainWindow, text="00", width=3, font=("Arial", 20), command=lambda: inClick("00")).place(x=40, y=337)
+Button(mainWindow, text="=", width=3,bg="#9575CD", foreground="#FFFFFF", relief="flat",  font=("Arial", 20), command= calculate).place(x=220, y=337)
+Button(mainWindow, text=".", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick(".")).place(x=160, y=337)
+Button(mainWindow, text="0", width=3, relief="flat", bg="#1E1E1E" ,foreground="#FFFFFF", font=("Arial", 20), command=lambda: Inclick("0")).place(x=100, y=337)
+Button(mainWindow, text="00", width=3, relief="flat",bg="#1E1E1E" ,foreground="#FFFFFF",  font=("Arial", 20), command=lambda: Inclick("00")).place(x=40, y=337)
 
+# To show the window
 mainWindow.mainloop()
