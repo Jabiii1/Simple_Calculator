e1846f7ffd1dd89635d3ee2265c580415bd0a367
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-23 10:33:09 +0800

    update

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -5,6 +5,13 @@ mainWindow.title("Simple Calculator")
 mainWindow.geometry("314x414")  
 mainWindow.configure(bg="lightblue")
 
+def button_click(value):
+    screen_var.set(screen_var.get() + str(value))
+
+# Screen variables
+screen_var = StringVar()
+screen2_var = StringVar()
+
 # Screens for calcu
 screen = Entry(mainWindow, font=("Arial", 35),width=12, relief="ridge", justify="right").grid(column=1,row=1, sticky="nsew")
 screen2 = Entry(mainWindow, font=("Arial", 15),width=12, relief="ridge", justify="right").grid(column=1,row=2, sticky="nsew")
@@ -14,23 +21,23 @@ mainWindow.iconphoto(False, icon)
 # Buttons for calcu
 
 #First Group
-erase = Button(mainWindow, text="⌫", font=("Arial", 20))
+erase = Button(mainWindow, text="⌫", font=("Arial", 20), command=lambda: button_click("⌫"))
 erase.place(x=220, y=105)
-nine = Button(mainWindow, text="9",width=3, font=("Arial", 20))
+nine = Button(mainWindow, text="9",width=3, font=("Arial", 20), command=lambda: button_click("9"))
 nine.place(x=160, y=105)
-eight = Button(mainWindow, text="8",width=3, font=("Arial", 20))
+eight = Button(mainWindow, text="8",width=3, font=("Arial", 20), command=lambda: button_click("8"))
 eight.place(x=100, y=105)
-seven = Button(mainWindow, text="7",width=3, font=("Arial", 20))        
+seven = Button(mainWindow, text="7",width=3, font=("Arial", 20), command=lambda: button_click("7"))        
 seven.place(x=40, y=105)
 
 # Second Goup
-divide = Button(mainWindow, text="÷", width=3, font=("Arial", 20))
+divide = Button(mainWindow, text="÷", width=3, font=("Arial", 20), command=lambda: button_click("÷"))
 divide.place(x=220, y=163)
-six = Button(mainWindow, text="6", width=3, font=("Arial", 20))
+six = Button(mainWindow, text="6", width=3, font=("Arial", 20), command=lambda: button_click("6"))
 six.place(x=160, y=163)
-five = Button(mainWindow, text="5",width=3, font=("Arial", 20))
+five = Button(mainWindow, text="5",width=3, font=("Arial", 20), command=lambda: button_click("5"))
 five.place(x=100, y=163)
-four = Button(mainWindow, text="4",width=3, font=("Arial", 20))
+four = Button(mainWindow, text="4",width=3, font=("Arial", 20), command=lambda: button_click("4"))
 four.place(x=40, y=163)
 
 # Third Group
