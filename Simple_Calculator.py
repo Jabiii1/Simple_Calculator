f2c5c04284c2d537415dcfb8cd9e6b2461fca799
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-23 11:48:58 +0800

    newwwwwwwwwwwww

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,19 +1,19 @@
 from tkinter import *
 
-def button_click(value):
+def inClick(value):
     screen_var.set(screen_var.get() + str(value))
 
-def clear():
+def clearLatest():
     current = screen_var.get()
     screen_var.set(current[:-1])
     
-def delete():
+def clearAll():
     screen_var.set("")
     screen2_var.set("")
 
 def calculate():
     try:
-        expression = screen_var.get().replace("x", "*").replace("÷", "/")
+        expression = screen_var.get().replace("×", "*").replace("÷", "/").replace("%", "/100")
         result = eval(expression)
         screen2_var.set(result)
     except:
@@ -23,6 +23,7 @@ mainWindow = Tk()
 mainWindow.title("Simple Calculator")
 mainWindow.geometry("314x415")  
 mainWindow.configure(bg="grey")
+mainWindow.resizable(False, False)
 
 # Screen variables
 screen_var = StringVar()
@@ -40,38 +41,33 @@ mainWindow.iconphoto(False, icon)
 # Buttons for calcu
 
 #First Group
+Button(mainWindow, text="÷",width=3, font=("Arial", 20), command=lambda: inClick("÷")).place(x=220, y=105)
+Button(mainWindow, text="%",width=3, font=("Arial", 20), command=lambda: inClick("%")).place(x=160, y=105)
+Button(mainWindow, text="⌦",width=3, font=("Arial", 20), command=lambda: clearLatest()).place(x=100, y=105)
+Button(mainWindow, text="C", width=3, font=("Arial", 20), command=lambda: clearAll()).place(x=40, y=105)
 
-Button(mainWindow, text="C", font=("Arial", 20), command=lambda: delete()).place(x=220, y=105)
-Button(mainWindow, text="⌫",width=3, font=("Arial", 20), command=lambda: clear()).place(x=160, y=105)
-Button(mainWindow, text="%",width=3, font=("Arial", 20), command=lambda: button_click("%")).place(x=100, y=105)
-Button(mainWindow, text="÷",width=3, font=("Arial", 20), command=lambda: button_click("÷")).place(x=40, y=105)
+# Second Goup
+Button(mainWindow, text="×",width=3, font=("Arial", 20), command=lambda: inClick("×")).place(x=220, y=163)
+Button(mainWindow, text="9",width=3, font=("Arial", 20), command=lambda: inClick("9")).place(x=160, y=163)
+Button(mainWindow, text="8",width=3, font=("Arial", 20), command=lambda: inClick("8")).place(x=100, y=163)
+Button(mainWindow, text="7",width=3, font=("Arial", 20), command=lambda: inClick("7")).place(x=40, y=163)
 
-# Button(mainWindow, text="⌫", font=("Arial", 20), command=lambda: clear()).place(x=220, y=105)
-# Button(mainWindow, text="9",width=3, font=("Arial", 20), command=lambda: button_click("9")).place(x=160, y=105)
-# Button(mainWindow, text="8",width=3, font=("Arial", 20), command=lambda: button_click("8")).place(x=100, y=105)
-# Button(mainWindow, text="7",width=3, font=("Arial", 20), command=lambda: button_click("7")).place(x=40, y=105)
+# Third Group
+Button(mainWindow, text="-", width=3, font=("Arial", 20), command=lambda: inClick("-")).place(x=220, y=221)
+Button(mainWindow, text="6", width=3, font=("Arial", 20), command=lambda: inClick("6")).place(x=160, y=221)
+Button(mainWindow, text="5",width=3, font=("Arial", 20), command=lambda: inClick("5")).place(x=100, y=221)
+Button(mainWindow, text="4",width=3, font=("Arial", 20), command=lambda: inClick("4")).place(x=40, y=221)
 
-# # Second Goup
-# Button(mainWindow, text="÷", width=3, font=("Arial", 20), command=lambda: button_click("÷")).place(x=220, y=163)
-# Button(mainWindow, text="6", width=3, font=("Arial", 20), command=lambda: button_click("6")).place(x=160, y=163)
-# Button(mainWindow, text="5",width=3, font=("Arial", 20), command=lambda: button_click("5")).place(x=100, y=163)
-# Button(mainWindow, text="4",width=3, font=("Arial", 20), command=lambda: button_click("4")).place(x=40, y=163)
+# Fourth Group
+Button(mainWindow, text="+", width=3, font=("Arial", 20), command=lambda: inClick("+")).place(x=220, y=279)
+Button(mainWindow, text="3", width=3, font=("Arial", 20), command=lambda: inClick("3")).place(x=160, y=279)
+Button(mainWindow, text="2", width=3, font=("Arial", 20), command=lambda: inClick("2")).place(x=100, y=279)
+Button(mainWindow, text="1", width=3, font=("Arial", 20), command=lambda: inClick("1")).place(x=40, y=279)
 
-# # Third Group
-# Button(mainWindow, text="x", width=3, font=("Arial", 20), command=lambda: button_click("x")).place(x=220, y=221)
-# Button(mainWindow, text="3", width=3, font=("Arial", 20), command=lambda: button_click("3")).place(x=160, y=221)
-# Button(mainWindow, text="2", width=3, font=("Arial", 20), command=lambda: button_click("2")).place(x=100, y=221)
-# Button(mainWindow, text="1", width=3, font=("Arial", 20), command=lambda: button_click("1")).place(x=40, y=221)
-
-# # Fourth Group
-# Button(mainWindow, text="+", width=3, font=("Arial", 20), command=lambda: button_click("+")).place(x=220, y=279)
-# Button(mainWindow, text="-", width=3, font=("Arial", 20), command=lambda: button_click("-")).place(x=160, y=279)
-# Button(mainWindow, text="0", width=3, font=("Arial", 20), command=lambda: button_click("0")).place(x=100, y=279)
-# Button(mainWindow, text=".", width=3, font=("Arial", 20), command=lambda: button_click(".")).place(x=40, y=279)
-
-# # Fifth
-# Button(mainWindow, text="=", width=12, font=("Arial", 20), command= calculate).place(x=58, y=337)
-
-
+# Fourth Group
+Button(mainWindow, text="=", width=3, font=("Arial", 20), command= calculate).place(x=220, y=337)
+Button(mainWindow, text=".", width=3, font=("Arial", 20), command=lambda: inClick(".")).place(x=160, y=337)
+Button(mainWindow, text="0", width=3, font=("Arial", 20), command=lambda: inClick("0")).place(x=100, y=337)
+Button(mainWindow, text="00", width=3, font=("Arial", 20), command=lambda: inClick("00")).place(x=40, y=337)
 
 mainWindow.mainloop()
